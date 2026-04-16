21b87d7a47bd28034205a00a7dea8c3212ba0454
Vinicius Bandeira <vbandeira@users.noreply.github.com>
2026-04-16 09:02:51 -0300

    Add: permutation-string - submission-8

---
+++ Data Structures & Algorithms/permutation-string/submission-8.py
@@ -0,0 +1,39 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+        
+        s1_freq = [0] * 26
+        s2_freq = [0] * 26
+
+        for i in range(len(s1)):
+            s1_freq[ord(s1[i]) - ord('a')] += 1
+            s2_freq[ord(s2[i]) - ord('a')] += 1
+        
+        matches = 0
+
+        for i in range(26):
+            matches += (1 if s1_freq[i] == s2_freq[i] else 0)
+
+        l = 0
+        for r in range(len(s1), len(s2)):
+            if matches == 26:
+                return True
+
+            idx = ord(s2[r]) - ord('a')
+            s2_freq[idx] += 1
+
+            if s1_freq[idx] == s2_freq[idx]:
+                matches += 1
+            elif s1_freq[idx] + 1 == s2_freq[idx]:
+                matches -= 1
+            
+            idx = ord(s2[l]) - ord('a')
+            s2_freq[idx] -= 1
+
+            if s1_freq[idx] == s2_freq[idx]:
+                matches += 1
+            elif s1_freq[idx] - 1 == s2_freq[idx]:
+                matches -= 1
+            l += 1
+        return matches == 26
