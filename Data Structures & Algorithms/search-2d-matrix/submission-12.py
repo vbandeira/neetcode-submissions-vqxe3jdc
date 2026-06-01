50f40981d4d7a1bebb8c6683ab812e8e8fe35af9
Vinicius Bandeira <vbandeira@users.noreply.github.com>
2026-06-01 08:37:14 -0300

    Add: search-2d-matrix - submission-12

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-12.py
@@ -0,0 +1,38 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        # Minha Solução 4: O(log m + log n) = O(log(m * n))
+            
+
+        l, r = 0, len(matrix) -1
+        row_index = -1
+        while l <= r:
+            mid = l + ((r - l) // 2)
+            if matrix[mid][0] <= target and target <= matrix [mid][-1]:
+                row_index = mid
+                break
+            elif matrix[mid][0] > target:
+                r = mid - 1
+            else:
+                l = mid + 1
+
+        if row_index == -1:
+            return False
+        roi = matrix[row_index]
+
+        # for i in range(len(matrix)):
+        #     if target >= matrix[i][0]:
+        #         roi = matrix[i]
+        
+        # if not roi:
+        #     return False
+
+        l, r = 0, len(roi) - 1
+        while l <= r:
+            mid = l + ((r - l) // 2)
+            if roi[mid] == target:
+                return True
+            if roi[mid] < target:
+                l = mid + 1
+            else:
+                r = mid - 1
+        return False
