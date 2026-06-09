7727e0ec39bb8d481889961dba69508a1b3ff29d
Vinicius Bandeira <vbandeira@users.noreply.github.com>
2026-06-09 13:56:53 -0300

    Add: time-based-key-value-store - submission-1

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-1.py
@@ -0,0 +1,26 @@
+# Busca binária
+
+class TimeMap:
+
+    def __init__(self):
+        self.values = defaultdict(list)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        # O(1)
+        self.values[key].append((value, timestamp))
+
+    def get(self, key: str, timestamp: int) -> str:
+        # O(log n)
+        values = self.values[key]
+        res = ''
+        l, r = 0, len(values) - 1
+        while l <= r:
+            mid = (l + r) // 2
+            v, t = values[mid]
+            if t <= timestamp:
+                l = mid + 1
+                res = v
+            else:
+                r = mid - 1
+        return res
+
