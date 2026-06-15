4023a281f15287d0b5291e37f0728343499d44d9
Vinicius Bandeira <vbandeira@users.noreply.github.com>
2026-06-15 16:15:26 -0300

    Add: find-median-in-a-data-stream - submission-4

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-4.py
@@ -0,0 +1,42 @@
+class MedianFinder:
+    # Minha solução: O(n log n)
+
+    def __init__(self):
+        self.minHeap = []   # for larger values
+        self.maxHeap = []   # for smaller values
+
+    def addNum(self, num: int) -> None:
+        # O(log n)
+
+        # Always add in max Heap
+        heapq.heappush(self.maxHeap, -num)
+
+        # if top of minHeap > top of maxHeap, move element 
+        #   from maxHeap to minHeap
+        if self.maxHeap and self.minHeap \
+            and -self.maxHeap[0] > self.minHeap[0]:
+            v = -heapq.heappop(self.maxHeap)
+            heapq.heappush(self.minHeap, v)
+
+        # if maxHeap len - minHeap len > 1, 
+        #   move element from maxHeap to minHeap
+        if len(self.maxHeap) - len(self.minHeap) > 1:
+            v = -heapq.heappop(self.maxHeap)
+            heapq.heappush(self.minHeap, v)
+        
+        # if minHeap len - maxHeap len > 1,
+        #   move elemnt from minHeap to maxHeap
+        if len(self.minHeap) - len(self.maxHeap) > 1:
+            v = -heapq.heappop(self.minHeap)
+            heapq.heappush(self.maxHeap, v)
+
+
+    def findMedian(self) -> float:
+        # O(1)
+
+        if len(self.minHeap) > len(self.maxHeap):
+            return self.minHeap[0]
+        elif len(self.maxHeap) > len(self.minHeap):
+            return -self.maxHeap[0]
+        else:
+            return (self.minHeap[0] + (-self.maxHeap[0])) / 2
